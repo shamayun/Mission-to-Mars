83198cae0794d442bfbedb135709da09a6cfddbc
Shamay Bahar <96354508+shamayun@users.noreply.github.com>
2022-03-05 18:43:05 -0500

    Updated Codes

--- scraping.py
+++ scraping.py
@@ -1,68 +1,74 @@
-
 # Import Splinter and BeautifulSoup
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 
-# set up the URL (NASA Mars News (Links to an external site.)) for scraping.
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+def mars_news(browser):
 
-# Visit the mars nasa news site
-url = 'https://redplanetscience.com'
-browser.visit(url)
-# Optional delay for loading the page
-browser.is_element_present_by_css('div.list_text', wait_time=1) #optional delay is useful, dynamic pages take longer to load
+    # Scrape Mars News
+    # Visit the mars nasa news site
+    url = 'https://redplanetscience.com/'
+    browser.visit(url)
 
-# I've assigned slide_elem as the variable to look for the <div /> tag and its descendent (the other tags within the <div /> element). This is parent element. This means that this element holds all of the other elements within it, and I'll reference it when we want to filter search results even further. The (.) is used for selecting classes, such as list_text, so the code 'div.list_text' pinpoints the <div /> tag with the class of list_text. CSS works from right to left, such as returning the last item on the list instead of the first. Because of this, when using select_one, the first matching element returned will be a <li /> element with a class of slide and all nested elements within it.
+    # Optional delay for loading the page
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
 
-#set up the HTML parser
-html = browser.html
-news_soup = soup(html, 'html.parser')
-slide_elem = news_soup.select_one('div.list_text')
+    # Convert the browser html to a soup object and then quit the browser
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
 
-#This variable holds a ton of information, so look inside of that information to find this specific data.
-slide_elem.find('div', class_='content_title') #The specific data is in a <div /> with a class of 'content_title'."
+    # Add try/except for error handling
+    try:
+        slide_elem = news_soup.select_one('div.list_text')
+        # Use the parent element to find the first 'a' tag and save it as 'news_title'
+        news_title = slide_elem.find('div', class_='content_title').get_text()
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
 
-# Use the parent element to find the first `a` tag and save it as `news_title`
-news_title = slide_elem.find('div', class_='content_title').get_text() # get_text(), only the text of the element is returned
-news_title
+    except AttributeError:
+        return None, None
 
-# Use the parent element to find the paragraph text
-news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-news_p
+    return news_title, news_p
 
-# Visit URL
-url = 'https://spaceimages-mars.com'
-browser.visit(url)
+    def featured_image(browser):
+    # Visit URL
+        url = 'https://spaceimages-mars.com'
+    browser.visit(url)
 
-# Find and click the full image button
-full_image_elem = browser.find_by_tag('button')[1] # browser to click the second button
-full_image_elem.click()
+    # Find and click the full image button
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
 
-# Parse the resulting html with soup
-html = browser.html
-img_soup = soup(html, 'html.parser')
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
 
-# Find the relative image url
-img_url_rel = img_soup.find('img', class_='headerimage fade-in').get('src')
-img_url_rel
+    # Add try/except for error handling
+    try:
+        # Find the relative image url
+        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
 
-# Use the base URL to create an absolute URL
-img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-img_url
+    except AttributeError:
+        return None
 
-# scrape the entire table with Pandas' .read_html() function
-df = pd.read_html('https://galaxyfacts-mars.com')[0] # index of 0, telling Pandas to pull only the first table/first item in list
-df.columns=['description', 'Mars', 'Earth']
-df.set_index('description', inplace=True)
-df
+    # Use the base url to create an absolute url
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
 
-# convert our DataFrame back into HTML-ready code
-df.to_html() # it's a <table /> element with a lot of nested elements
+    return img_url
 
-# turning off the browser
-browser.quit()
+    def mars_facts():
+    # Add try/except for error handling
+        try:
+        # Use 'read_html' to scrape the facts table into a dataframe
+            df = pd.read_html('https://galaxyfacts-mars.com')[0]
 
+        except BaseException:
+            return None
 
+    # Assign columns and set index of dataframe
+    df.columns=['Description', 'Mars', 'Earth']
+    df.set_index('Description', inplace=True)
+
+    # Convert dataframe into HTML format, add bootstrap
+    return df.to_html()
